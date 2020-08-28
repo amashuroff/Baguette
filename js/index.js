ad6209464be04ff12e2e8a0f54dd8d4d44d16f94
Aleksei <amashuroff@gmail.com>
2020-08-28 20:52:08 +0300

    changes to menu, gallery

--- js/index.js
+++ js/index.js
@@ -1,47 +1,126 @@
 
 // add photos to gallery
-const galleryItems = ['item-1', 'item-2', 'item-3', 'flex-4','item-5' ,'item-6', 'item-7'];
+const galleryItems = ['item-1', 'item-2', 'item-3', 'flex-4','item-5' ,'item-6', 'item-7', 'item-8','item-9', 'item-10', 'item-11', 'item-12', 'item-13', 'item-14', 'item-15', 'item-16' ];
 
 const addGalleryItem = (index, photo) => {
     const d = document.createElement('div');
     d.className = `item-${index}`;
-    d.innerHTML = `<img src="./css/images/${photo}.jpg" alt="Фото из Галереи" class="item">`
+    d.style.position = 'relative';
+    d.style.zIndex = 5;
+    d.innerHTML = `
+    <img src="./css/images/${photo}.jpg" alt="Фото из Галереи" class="item">
+    <div class="item-hover"></div>
+    `;
 
     document.querySelector('.gallery-container').appendChild(d);
 };
 
+const addHoverListener = (index) => {
+    document.querySelector(`.item-${index}`).addEventListener('mouseover', () => {
+        let itemHover = document.querySelector(`.item-${index} .item-hover`).style;
+        itemHover.opacity = 1;  
+    });
+    document.querySelector(`.item-${index}`).addEventListener('mouseleave', () => {
+        let itemHover = document.querySelector(`.item-${index} .item-hover`).style;
+        itemHover.opacity = 0;
+    })
+}
+
 for (let i = 0; i < galleryItems.length; i++) {
     addGalleryItem(i+1, galleryItems[i]);
+    addHoverListener(i+1);
 }
 
 
-// create gallery
+
+// create photoswipe gallery
 var pswpElement = document.querySelectorAll('.pswp')[0];
 
 var items = [
     {
-        src: '/images/1.jpeg',
-        w: 1200,
-        h: 900
+        src: '../css/images/item-1.jpg',
+        h: 600,
+        w: 360
     },
     {
-        src: 'https://placekitten.com/600/400',
-        w: 600,
-        h: 400
+        src: '../css/images/item-2.jpg',
+        h: 600,
+        w: 360
     },
     {
-        src: '/images/2.jpeg',
-        w: 1200,
-        h: 900
+        src: '../css/images/item-3.jpg',
+        h: 600,
+        w: 360
     },
     {
-        src: '/images/3.jpg',
-        w: 1200,
-        h: 900
-    }
+        src: '../css/images/flex-4.jpg',
+        h: 600,
+        w: 360
+    },
+    {
+        src: '../css/images/item-5.jpg',
+        h: 600,
+        w: 360
+    },
+    {
+        src: '../css/images/item-6.jpg',
+        h: 600,
+        w: 360
+    },
+    {
+        src: '../css/images/item-7.jpg',
+        h: 600,
+        w: 360
+    },
+    {
+        src: '../css/images/item-8.jpg',
+        h: 600,
+        w: 360
+    },
+    {
+        src: '../css/images/item-9.jpg',
+        h: 600,
+        w: 360
+    },
+    {
+        src: '../css/images/item-10.jpg',
+        h: 600,
+        w: 360
+    },
+    {
+        src: '../css/images/item-11.jpg',
+        h: 600,
+        w: 360
+    },
+    {
+        src: '../css/images/item-12.jpg',
+        h: 600,
+        w: 360
+    },
+    {
+        src: '../css/images/item-13.jpg',
+        h: 600,
+        w: 360
+    },
+    {
+        src: '../css/images/item-14.jpg',
+        h: 600,
+        w: 360
+    },
+    {
+        src: '../css/images/item-15.jpg',
+        h: 600,
+        w: 360
+    },
+    {
+        src: '../css/images/item-16.jpg',
+        h: 600,
+        w: 360
+    },
+
 ];
 
-var thumbnails = document.querySelectorAll('.item');
+var thumbnails = document.querySelectorAll('.item-hover');
 
 
 thumbnails.forEach((el,i) => {
