d7ee27b91628ec80771bf67ac1c091150e97a5c9
Aleksei <amashuroff@gmail.com>
2020-08-30 19:21:48 +0300

    resize img

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 
+
 // add photos to gallery
 const galleryItems = ['item-1', 'item-2', 'item-3', 'flex-4','item-5' ,'item-6', 'item-7', 'item-8','item-9', 'item-10', 'item-11', 'item-12', 'item-13', 'item-14', 'item-15', 'item-16' ];
 const galleryArr = [];
